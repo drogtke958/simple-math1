67521c20666b17880196f83debe2176fd9ec3194
drogtke958 <153301526+drogtke958@users.noreply.github.com>
2024-09-25 09:50:45 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+# Keily Drogt
+#25 Sept 2024
+# Simple math 1
 
+# Create and assign a numeric value to each variable
+num1 = 5
+num2 = 2
+
+# Formula to do the multiplication
+product = num1 * num2
+
+# Show the output (answer)
+print(f'{num1} times {num2} = {product}')
