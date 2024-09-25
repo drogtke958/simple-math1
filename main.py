ca91dae087738fc90a702be31d00f78527603954
drogtke958 <153301526+drogtke958@users.noreply.github.com>
2024-09-25 09:54:38 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 #25 Sept 2024
 # Simple math 1
 
+#Practice 1
 # Create and assign a numeric value to each variable
 num1 = 5
 num2 = 2
@@ -11,3 +12,7 @@ product = num1 * num2
 
 # Show the output (answer)
 print(f'{num1} times {num2} = {product}')
+
+# Practice 2
+length = float(input("Enter the lenght on your room in feet: "))
+width = float(input("Enter the width of your room in feet: "))
